e96e3b495ce63e82dc506ef28632929b49facc5f
skylergrandel <skyler.h.grandel@vanderbilt.edu>
2023-10-06 12:56:44 -0500

    Added output.json functionality

--- benchmark_framework.py
+++ benchmark_framework.py
@@ -2,9 +2,10 @@ import json
 import importlib
 import glob
 import sys
-import os  # Import the os module to join paths
+import os
 
 from llm_test_helpers import get_llm, get_args
+
 args = get_args(sys.argv)
 llm = get_llm(args.model)
 
@@ -16,40 +17,48 @@ def load_benchmarks(directory):
 def run_benchmark(benchmark, ai_module):
     total_points_earned = 0
     total_points_available = 0
+    results = []
     
     for problem in benchmark['problems']:
         prompt = problem['prompt']
         solution = llm.predict(prompt)
-
-        # Construct the path to the test suite module
         test_suite_path = ".".join(["test_suites", benchmark['name'], problem['test_suite'][:-3]])
         
         try:
             test_suite_module = importlib.import_module(test_suite_path)
             points_earned, total_points = test_suite_module.test(solution)
-        except:
-            print("Exception while testing")
-
-        print(points_earned, "/", total_points)
+        except Exception as e:
+            print("Exception while testing:", e)
+            points_earned, total_points = 0, 0  # assuming you award 0 for exceptions
+        
+        results.append({
+            "points_earned": points_earned,
+            "total_points": total_points
+        })
         
         total_points_earned += points_earned
         total_points_available += total_points
     
-    score = total_points_earned / total_points_available * 100
-    return score
+    score = total_points_earned / total_points_available * 100 if total_points_available != 0 else 0
+    return score, results
 
 def main():
-    #ai_module = importlib.import_module(ai_filename[:-3])
     ai_module = llm
     benchmarks = load_benchmarks('./benchmarks')
     
+    output_data = []
+    
     for benchmark in benchmarks:
-        score = run_benchmark(benchmark, ai_module)
+        score, results = run_benchmark(benchmark, ai_module)
+        output_data.append({
+            "benchmark_name": benchmark['name'],
+            "score": score,
+            "results": results
+        })
         print(f"{benchmark['name']}: {score}%")
+        
+    with open('output.json', 'w') as f:
+        json.dump(output_data, f, indent=4)
 
-
-main()
-"""
 if __name__ == "__main__":
-    main(sys.argv[1])
-"""
+    main()
